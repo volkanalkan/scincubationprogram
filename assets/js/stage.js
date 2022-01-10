f2624eacfaba575a2ea81efd12d2b5fda1cf6874
Volkan Alkan <volkanalkan2001@hotmail.com>
2022-01-10 17:06:45 +0300

    Initial commit

---
+++ assets/js/stage.js
@@ -0,0 +1,100 @@
+    
+    $(".step").click( function() {
+        $(this).addClass("active").prevAll().addClass("active");
+        $(this).nextAll().removeClass("active");
+    });
+    
+    $(".step01").click( function() {
+        $("#line-progress").css("width", "3%");
+        $(".discovery").addClass("active").siblings().removeClass("active");
+    });
+    
+    $(".step02").click( function() {
+        $("#line-progress").css("width", "25%");
+        $(".strategy").addClass("active").siblings().removeClass("active");
+    });
+    
+    $(".step03").click( function() {
+        $("#line-progress").css("width", "50%");
+        $(".creative").addClass("active").siblings().removeClass("active");
+    });
+
+
+
+
+        
+    $(".stepa").click( function() {
+        $(this).addClass("active").prevAll().addClass("active");
+        $(this).nextAll().removeClass("active");
+    });
+    
+    $(".step001").click( function() {
+        $("#line-progress").css("width", "3%");
+        $(".discovery1").addClass("active").siblings().removeClass("active");
+    });
+    
+    $(".step002").click( function() {
+        $("#line-progress").css("width", "25%");
+        $(".strategy2").addClass("active").siblings().removeClass("active");
+    });
+    
+    $(".step003").click( function() {
+        $("#line-progress").css("width", "50%");
+        $(".creative3").addClass("active").siblings().removeClass("active");
+    });
+
+
+
+
+
+        
+    $(".stepb").click( function() {
+        $(this).addClass("active").prevAll().addClass("active");
+        $(this).nextAll().removeClass("active");
+    });
+    
+    $(".step004").click( function() {
+        $("#line-progress").css("width", "3%");
+        $(".discovery4").addClass("active").siblings().removeClass("active");
+    });
+    
+    $(".step005").click( function() {
+        $("#line-progress").css("width", "25%");
+        $(".strategy5").addClass("active").siblings().removeClass("active");
+    });
+    
+    $(".step006").click( function() {
+        $("#line-progress").css("width", "50%");
+        $(".creative6").addClass("active").siblings().removeClass("active");
+    });
+     
+
+
+
+
+
+    $(".stepc").click( function() {
+        $(this).addClass("active").prevAll().addClass("active");
+        $(this).nextAll().removeClass("active");
+    });
+    
+    $(".step007").click( function() {
+        $("#line-progress").css("width", "3%");
+        $(".discovery7").addClass("active").siblings().removeClass("active");
+    });
+    
+    $(".step008").click( function() {
+        $("#line-progress").css("width", "25%");
+        $(".strategy8").addClass("active").siblings().removeClass("active");
+    });
+    
+    $(".step009").click( function() {
+        $("#line-progress").css("width", "50%");
+        $(".creative9").addClass("active").siblings().removeClass("active");
+    });
+
+    $(".step010").click( function() {
+        $("#line-progress").css("width", "50%");
+        $(".creative10").addClass("active").siblings().removeClass("active");
+    });
+     
